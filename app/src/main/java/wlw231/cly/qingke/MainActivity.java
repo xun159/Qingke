c9860c4aa7033eac2d57b0a1e52e09935678dafc
xunfeng <3415431250@qq.com>
2026-04-17 20:46:50 +0800

    logUI布局

---
+++ app/src/main/java/wlw231/cly/qingke/MainActivity.java
@@ -0,0 +1,14 @@
+package wlw231.cly.qingke;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInter){
+        super.onCreate(savedInter);
+    }
+}
